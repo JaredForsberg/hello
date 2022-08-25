650b788d4ee34eec571a290ce7260eb3e441ba8e
Jared <jforsberg2@huskers.unl.edu>
2022-08-25 18:55:48 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Jared Forsberg
+ * Date: 2022/08/25
+ * A file that prints my name
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Jared Forsberg!\n");
+    printf("Computer Engineering\n");
+    return 0;
+}
